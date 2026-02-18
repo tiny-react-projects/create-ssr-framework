634422605a8de5ef825bbe3a6f70668b76358ace
yunajoe <yunajoe@gmail.com>
2026-02-18 11:57:25 +0900

    feat: clinet와 server 분리

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "https://esm.sh/react-dom@18/client";
-
-function App() {
-  return <div>App컴퍼넌트입니다.</div>;
-}
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>App컴퍼넌트입니다.</div>;
+}
+
+export default App;
